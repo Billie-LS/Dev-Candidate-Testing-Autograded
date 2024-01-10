9d9586edb8584842082ddbe77cfcbd87cfcda56f
Billie-LS <lskylizard@yahoo.com>
2024-01-09 23:44:12 -0600

    minimum viable quiz

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,13 +1,18 @@
+// Import the readline-sync module for user input
 const input = require('readline-sync');
 
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+// Define candidateName to empty string
+let candidateName = '';
+
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+// Prompt the candidate for answer to first 'quiz question' 
+let question = input.question("Who was the first American woman in space? ");
+let correctAnswer = "Sally Ride"
+// set candidateAnswer to empty string
+let candidateAnswer = '';
 
 
 //TODO: Variables for Part 2
@@ -18,6 +23,7 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
+  candidateName = input.question("Enter your name: ");
 
 }
 
@@ -42,7 +48,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log();
+   console.log(`Hello ${candidateName}, I hope you have studied for this quiz!`);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
